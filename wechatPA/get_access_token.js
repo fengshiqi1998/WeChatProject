784c835233d25c10b0e6b4b7586fe58a965b6005
fengshiqi1998 <512653808@qq.com>
2019-03-18 19:36:15 +0800

    date 2019/3/18/19:36

--- wechatPA/get_access_token.js
+++ wechatPA/get_access_token.js
@@ -1,16 +1,79 @@
 const awyhttp = require('awyhttp');
-const wxkey = require('./weixinkey.js');
+const fs = require('fs');
+var wxkey = require('./weixinkey.js');
+
+const token_path = 'tmp/access_token.txt';
 
 var tokenApi = {
+	token_api: `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${wxkey.appid}&secret=${wxkey.appsecret}`,
 	getToken: function() {
-		var token_api = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${wxkey.appid}&secret=${wxkey.appsecret}`;
-		awyhttp.get(token_api).then(data => {
-		    console.log(data);
-		    return data;
-		}, err => {
-		    console.log(err);
-		    return err;
+		return new Promise((rv) => {
+			fs.exists(token_path, (exists) => {
+				if(exists) {
+					fs.readFile(token_path, {encoding:'utf8'}, (err, data) => {
+						console.log('it has read');
+						if (err) {
+							console.log('err');
+                    		rv(null);
+                		} else if(!data) {
+                			console.log('data is null');
+                			rv(null);
+                		} else {
+                			console.log('data:', data);
+	                    	var t = JSON.parse(data);
+	                    	console.log('t:',t);
+		                    var now_tm = (new Date()).getTime();
+		                    if (parseInt(t.get_time) + parseInt(t.expires_in) > parseInt(now_tm/1000)) {
+		                        rv(t.access_token);
+		                    } else {
+		                        rv(null);
+		                    }
+                		}
+					});
+				} else {
+					rv(null);
+				}
+			});
+		}).then(data => {
+			console.log('data:',data);
+			if(data) {
+				console.log('data has been ..');
+				return {
+                    status : true,
+                    data : data
+                };
+			}
+			console.log('has been data ...');
+			return awyhttp.get(this.token_api)
+            .then(data => {
+            	console.log('awyhttp has been')
+                var ret = JSON.parse(data);
+                if (ret.errcode === undefined) {
+                    ret.get_time = parseInt((new Date()).getTime()/1000);
+                    fs.writeFile(token_path, 
+                        JSON.stringify(ret),
+                        	{encoding:'utf8'},(err) => {
+                        }
+                    );
+                    
+                    return {
+                        status : true,
+                        data : ret.access_token
+                    };
+                } else {
+                    return {
+                        status : false,
+                        data   : data
+                    };
+                }
+            }, err => {
+                throw err;
+            });
+		}).catch(reason => {
+			console.log(reason);
 		});
+		
 	}
 }
+
 module.exports = tokenApi;
---
+++ wechatPA/set_menu.js
@@ -0,0 +1,68 @@
+const awyhttp = require('awyhttp');
+var wxkey = require('./weixinkey.js');
+var tokenApi = require('./get_access_token.js');
+
+var appid = wxkey.appid;
+var appsecret = wxkey.appsecret;
+
+var token_api = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${appsecret}`;
+
+var menu_data = {
+    button : [
+        {
+            name : "科技",
+            sub_button : [
+                {
+                    name : "Linux",
+                    type : "view",
+                    url  : "https://www.linux.org/"
+                },
+                {
+                    name : "NodeDoc",
+                    type : "view",
+                    url  : "https://nodejs.org/dist/latest-v10.x/docs/api/"
+                },
+                {
+                    name : "awy",
+                    type : "view",
+                    url  : "https://awy.linuslinux.com"
+                },
+
+            ]
+        },
+        {
+            name : "发图",
+            type : "pic_weixin",
+            key  : "my-image"
+        },
+        {
+            name : "功能",
+            sub_button : [
+                {
+                    name : "关于我们",
+                    type : "click",
+                    key  : "about-us"
+                },
+
+            ]
+        }
+        
+    ]
+};
+
+tokenApi.getToken().then(ret => {
+    var json_menu = JSON.stringify(menu_data);
+    console.log(ret);
+    var create_menu_api = `https://api.weixin.qq.com/cgi-bin/menu/create?access_token=${ret.data}`;
+
+    return awyhttp.post(create_menu_api, {
+        data : json_menu,
+        headers : {
+            'Content-Type'  : 'text/plain'
+        }
+    }).then(data => {
+        console.log(data);
+    }, err => {
+        console.log(err);
+    });
+});
