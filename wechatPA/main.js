1bc5d2692cc7c60c5e57ef11dd9b8c4557a3e84d
fengshiqi1998 <512653808@qq.com>
2019-04-01 16:41:18 +0800

    update ant.run => ant.ants

--- wechatPA/main.js
+++ wechatPA/main.js
@@ -5,5 +5,5 @@ var oauth = require('./wxoauth/wxoauth.js');
 /*
     请把8200换成自己的端口号，2表示创建两个子进程处理请求，默认会根据CPU核心数量创建。
 */
-// ant.ants('127.0.0.1', 8000, 2);
-ant.run('localhost', 8000);
+ant.ants('127.0.0.1', 8000, 2);
+//ant.run('localhost', 8000);
